d45e9b1d417569372e2e70cc9d0eef4bc823cc19
Allen Keettikkal <allenkeettikkal@gmail.com>
2021-05-15 18:41:58 -0600

    Completed a1q2.c and its output

--- a1q2.c
+++ a1q2.c
@@ -4,3 +4,17 @@
     Student Number: 11278995
  */
 
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0, remainder;
+    printf("The secret passcode: \n");
+    scanf("%d", &n);
+    while (n != 0) {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    printf("Enter this to enter the door: %d", rev);
+    return 0;
+}
