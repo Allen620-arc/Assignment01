093f8b069c2d53afe8ff35898532796f32ce3ec5
Allen Keettikkal <allenkeettikkal@gmail.com>
2021-05-22 13:53:56 -0600

    Finished a1q3.c with only collatz()

--- a1q3.c
+++ a1q3.c
@@ -6,6 +6,27 @@
 
 #include <stdio.h>
 
-int main() {
+int collatz(int n) {
+    int steps = 0;
+    while (n > 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        }
+        else {
+            n = 3 * n + 1;
+        }
+        steps += 1;
+    }
+    printf("The value received was %d after %d steps\n", n, steps);
+    return 0;
+}
 
+int main() {
+    int num;
+
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+
+    collatz(num);
+    return 0;
 }
