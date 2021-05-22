322f00dcaf70479d01edf5434a0aed572e6af51d
Allen Keettikkal <allenkeettikkal@gmail.com>
2021-05-22 13:58:49 -0600

    Finished run_collatz() in a1q3.c

--- a1q3.c
+++ a1q3.c
@@ -7,18 +7,22 @@
 #include <stdio.h>
 
 int collatz(int n) {
+    if (n % 2 == 0) {
+        n = n / 2;
+    }
+    else {
+        n = 3 * n + 1;
+    }
+    return 0;
+}
+
+int run_collatz(int n) {
     int steps = 0;
-    while (n > 1) {
-        if (n % 2 == 0) {
-            n = n / 2;
-        }
-        else {
-            n = 3 * n + 1;
-        }
+    if (n > 1) {
+        collatz(n);
         steps += 1;
     }
     printf("The value received was %d after %d steps\n", n, steps);
-    return 0;
 }
 
 int main() {
